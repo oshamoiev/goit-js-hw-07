0f055b417ead60399c9329a586f67aff3400dc83
Oleksandr Shamoiev <oleksandr.shamoiev@pitchbook.com>
2026-04-03 22:44:55 +0300

    Task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+const getRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+const handleChangeColorBtnClick = () => {
+  const randomColor = getRandomHexColor();
+  colorSpan.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+};
+
+changeColorBtn.addEventListener("click", handleChangeColorBtnClick)
