b2cdc4f7ac01d36c0a038e6212092eead86da8e2
Oleksandr Shamoiev <oleksandr.shamoiev@pitchbook.com>
2026-04-03 22:34:19 +0300

    Task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
 
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  const formElements = form.elements;
+  const formData = {};
+
+  for (let element of formElements) {
+    if (element.name) {
+      const value = element.value.trim();
+
+      if (!value) {
+        alert("All form fields must be filled in");
+        return;
+      }
+
+      formData[element.name] = value;
+    }
+  }
+
+  console.log(formData);
+  form.reset();
+};
+
+form.addEventListener("submit", handleFormSubmit);
